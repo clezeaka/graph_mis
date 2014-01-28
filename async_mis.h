a3b49d5e3e80e3b7b82e7cba86d0c8e657ce877c
clezeaka <clezeaka@gmail.com>
2014-01-27 21:22:40 -0500

    adding async_mis.h

---
+++ async_mis.h
@@ -0,0 +1,60 @@
+#ifndef ASYNC_MIS_H_
+#define ASYNC_MIS_H_
+
+#include "globals.h"
+#include "tournament.cpp"
+
+int compute_mis_tournament(Vertex *__vertices, unsigned char *__tournamentArray, ordering_method_t ordering_method) {
+    unsigned int numEdges =
+        (unsigned int) sparse_rep.Starts[sparse_rep.numRows];
+    //Vertex *__vertices = (Vertex *) malloc(sizeof(Vertex)*sparse_rep.numRows);
+    //unsigned char *__tournamentArray = (unsigned char *) malloc(numEdges);
+    cilk_for( unsigned int i = 0; i < sparse_rep.numRows;
+        i++ ) { // cilk_for
+        __vertices[i].vertex_init(__vertices, __tournamentArray, i,
+            (unsigned int *) sparse_rep.ColIds,
+            sparse_rep.Starts[i],
+            sparse_rep.Starts[i + 1]
+            - sparse_rep.Starts[i],
+            ordering_method);
+    }
+    cilk_for( size_t i = 0; i < sparse_rep.numRows; ++i) {
+        if (__vertices[i].numPredecessors == 0){
+            int degree =
+                sparse_rep.Starts[i + 1]
+                - sparse_rep.Starts[i];
+            __vertices[i].color_vertex(
+                __vertices,
+                __tournamentArray,
+                (unsigned int *) sparse_rep.ColIds,
+                1L);
+        }
+    }
+
+    //  cilk_for( int i = 0; i < sparse_rep.numRows; i++ ) { // cilk_for
+    //    if (__vertices[i].numPredecessors
+    //        == 0) {
+    //      int degree =
+    //          sparse_rep.Starts[i + 1]
+    //                            - sparse_rep.Starts[i];
+    //      __vertices[i].color_vertex(
+    //          __vertices,
+    //          __tournamentArray,
+    //          (unsigned int *) sparse_rep.ColIds,
+    //          1L);
+    //    }
+    //  }
+    // MERELY COPYING THE COLORS INTO THIS OTHER ARRAY TAKES ~10-15% of the time!!!
+    // THIS ISN'T REALLY FAIR - PERHAPS WE SHOULD VALIDATE THE COLORING ON THE
+    // __vertices[] ARRAY.
+    //for( int i = 0; i < sparse_rep.numRows; i++ ) { // cilk_for
+    //  vertexColors[i] = __vertices[i].finalColor;
+    //}
+
+    //free(__vertices);
+    //free(__tournamentArray);
+    return 1;
+}
+
+
+#endif /* ASYNC_MIS_H_ */
